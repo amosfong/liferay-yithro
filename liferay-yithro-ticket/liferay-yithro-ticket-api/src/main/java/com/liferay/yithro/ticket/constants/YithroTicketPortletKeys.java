a65303e479bac0dc1f622ed3dd72bacb1c94f40d
amosfong <amos.fong@liferay.com>
2019-04-30 14:32:06 -0700

    My Requested and Assigned Tickets Portlets SF

--- liferay-yithro-ticket/liferay-yithro-ticket-api/src/main/java/com/liferay/yithro/ticket/constants/YithroTicketPortletKeys.java
+++ liferay-yithro-ticket/liferay-yithro-ticket-api/src/main/java/com/liferay/yithro/ticket/constants/YithroTicketPortletKeys.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *
- *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.liferay.yithro.ticket.constants;
@@ -22,10 +22,10 @@ public class YithroTicketPortletKeys {
 	public static final String ADD_TICKET_FORM =
 		"com_liferay_yithro_ticket_constants_AddTicketFormPortlet";
 
-	public static final String MY_REQUESTED_TICKETS =
-		"com_liferay_yithro_ticket_constants_MyRequestedTickets";
-
 	public static final String MY_ASSIGNED_TICKETS =
 		"com_liferay_yithro_ticket_constants_MyAssignedTickets";
+
+	public static final String MY_REQUESTED_TICKETS =
+		"com_liferay_yithro_ticket_constants_MyRequestedTickets";
 
 }
